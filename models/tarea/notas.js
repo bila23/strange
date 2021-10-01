3ef745a8cac354439fbb8f33640fe66147e1e8b6
Bila <wgbila@gmail.com>
2021-10-01 17:08:43 -0600

    creacion modelo notas

---
+++ models/tarea/notas.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const Schema = mongoose.Schema;
+
+const notasSchema = new mongoose.Schema({
+  usuario: [{ type: Schema.Types.ObjectId, ref: "usuario" }],
+  fecha: Date,
+  total: Number,
+  finalizadas: Number,
+  pendientes: Number,
+});
+
+const Notas = mongoose.model("notas", notasSchema);
+
+exports.Notas = Notas;
