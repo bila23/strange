ac3607bb29d53670090d4bf46b0049a436051e15
Bila <wgbila@gmail.com>
2021-10-01 16:52:35 -0600

    creacion modelo bitacora estado

---
+++ models/bitacoraEstado.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bitacoraEstadoSchema = new mongoose.Schema({
+  tarea: [{ type: Schema.Types.ObjectId, ref: "tarea" }],
+  estadoAntiguo: String,
+  estadoNuevo: String,
+  usuario_crea: String,
+  fecha_crea: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const BitacoraEstado = mongoose.model("bitacoraEstado", bitacoraEstadoSchema);
+
+exports.BitacoraEstado = BitacoraEstado;
