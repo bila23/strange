362d419978944c120ab7eefb56651df4e3d373a1
Bila <wgbila@gmail.com>
2021-10-01 11:32:56 -0600

    modificacion de conexion a base mongodb

--- start/db.js
+++ start/db.js
@@ -2,9 +2,5 @@ const mongoose = require("mongoose");
 const { MONGO_URL } = require("./db_url");
 
 module.exports = function () {
-  mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  });
+  mongoose.connect(MONGO_URL);
 };
