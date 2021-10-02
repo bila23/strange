0c6c9e295fc92bfa71db96860bd8b8f23cbf392b
Bila <wgbila@gmail.com>
2021-10-02 09:13:42 -0600

    creacion de router tarea

---
+++ routes/tarea/tarea.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const { Tarea, validateTarea } = require("../../models/tarea/tarea");
+
+router.get("/oficina/:id", auth, async (req, res) => {
+  const list = await Tarea.find({ oficina: req.params.id }).sort({
+    fecha: 1,
+  });
+  res.send(list);
+});
+
+router.get("/responsable/:id", auth, async (req, res) => {
+  const list = await Tarea.find({ responsable: req.params.id }).sort({
+    fecha: 1,
+  });
+  res.send(list);
+});
+
+router.get("/:id", auth, async (req, res) => {
+  const model = await Tarea.findById(req.params.id);
+  if (!model) return res.status(400).send("No se encontró el registro");
+  res.send(model);
+});
+
+router.post("/", auth, async (req, res) => {
+  const { error } = validateTarea(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let model = new Tarea(req.body);
+  model = await model.save();
+
+  res.send(model);
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const conditions = { _id: req.params.id };
+  const updateField = {
+    descripcion: req.body.descripcion,
+    fecha: req.body.fecha,
+    responsable: req.body.responsable,
+    estado: req.body.estado,
+  };
+  const model = await Tarea.updateOne(conditions, updateField);
+  if (!model)
+    return res
+      .status(400)
+      .send("No se encontró el registro que se desea actualizar");
+  res.send(model);
+});
+
+module.exports = router;
