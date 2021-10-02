2b91f47b5f6ec1407956834472f0fbb850180d24
Bila <wgbila@gmail.com>
2021-10-01 18:07:02 -0600

    creacion de api's auth y user

--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,9 @@
 const error = require("../middleware/error");
+const usuario = require("../routes/tarea/usuario");
+const auth = require("../routes/auth/auth");
 
 module.exports = function (app) {
+  app.use("/usuario", usuario);
+  app.use("/auth", auth);
   app.use(error);
 };
