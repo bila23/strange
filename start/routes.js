2b9ca64e542d640badc47da189f7939645f6655d
Bila <wgbila@gmail.com>
2021-10-02 09:15:57 -0600

    se agregaron nuevos endpoint

--- start/routes.js
+++ start/routes.js
@@ -1,8 +1,14 @@
 const error = require("../middleware/error");
 const usuario = require("../routes/tarea/usuario");
+const tarea = require("../routes/tarea/tarea");
+const observacionTarea = require("../routes/tarea/observacionTarea");
+const bitacoraEstado = require("../routes/tarea/bitacoraEstado");
 const auth = require("../routes/auth/auth");
 
 module.exports = function (app) {
+  app.use("/bitacora-estado", bitacoraEstado);
+  app.use("/observacion-tarea", observacionTarea);
+  app.use("/tarea", tarea);
   app.use("/usuario", usuario);
   app.use("/auth", auth);
   app.use(error);
