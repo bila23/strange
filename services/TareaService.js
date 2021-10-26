e4896e6c36f20cc3ab944858d8ded8cb2f52cd81
Bila <wgbila@gmail.com>
2021-10-25 19:33:30 -0600

    ...

--- services/TareaService.js
+++ services/TareaService.js
@@ -1,9 +1,18 @@
-const { sendMail } = require("../util/mail");
+const { sendMail, sendMailWithCC } = require("../util/mail");
+const UsuarioService = require("./UsuarioService");
 
 async function sendMailToSave() {
   try {
+    const senders = await UsuarioService.findUserAuthorize();
     const html = `Se ha ingresado una nueva tarea para su revision, favor ingresar al sistema y verificar.<br/><a href="https://jarvis-alpha.vercel.app">[Clic para ir al sistema]</a>`;
-    await sendMail("wgbila@gmail.com", "INGRESO DE NUEVA TAREA", html);
+    if (!senders || senders.length === 0) return false;
+
+    const length = senders.length;
+    const title = "INGRESO DE NUEVA TAREA";
+
+    if (length === 1) await sendMail(senders[0], title, html);
+    else await sendMailWithCC(senders[0], title, html, senders);
+
     return true;
   } catch (e) {
     return false;
