2aa889f7569725bf67d56cc335baf4c3f25b6c54
Bila <wgbila@gmail.com>
2021-10-28 13:20:19 -0600

    ....

--- routes/tarea/tarea.js
+++ routes/tarea/tarea.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { Tarea, validateTarea } = require("../../models/tarea/tarea");
+const { BitacoraEstado } = require("../../models/tarea/bitacoraEstado");
 const TareaService = require("../../services/TareaService");
 
 router.get("/ingresadas", auth, async (req, res) => {
