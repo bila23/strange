fd99f0c64951f0df832c38b5b5f6e56471ce9574
Bila <wgbila@gmail.com>
2021-10-28 14:20:32 -0600

    ...

--- routes/tarea/tarea.js
+++ routes/tarea/tarea.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const { Tarea, validateTarea } = require("../../models/tarea/tarea");
 const { BitacoraEstado } = require("../../models/tarea/bitacoraEstado");
+const { Usuario } = require("../../models/tarea/usuario");
 const TareaService = require("../../services/TareaService");
 
 router.get("/ingresadas", auth, async (req, res) => {
@@ -47,16 +48,39 @@ router.post("/", auth, async (req, res) => {
   res.send(model);
 });
 
-router.put("/autorizar/:id", auth, async (req, res) => {
+router.put("/autorizar/:id/:user", auth, async (req, res) => {
+  const usuario = await Usuario.findOne({ user: req.params.user });
+
+  if (!usuario)
+    return res
+      .status(400)
+      .send("No se encontró el usuario que debe autorizar la tarea");
+
   const conditions = { _id: req.params.id };
   const updateField = {
+    descripcion: req.body.descripcion,
+    fecha: req.body.fecha,
+    responsable: req.body.responsable,
     estado: "APROBADA",
+    autoriza: usuario._id,
   };
   const model = await Tarea.updateOne(conditions, updateField);
+
   if (!model)
     return res
       .status(400)
       .send("No se encontró el registro que se desea actualizar");
+
+  const bitacoraModel = {
+    tarea: model._id,
+    estadoAntiguo: "INGRESADA",
+    estadoNuevo: "APROBADA",
+    usuario_crea: req.params.user,
+  };
+
+  const bitacora = new BitacoraEstado(bitacoraModel);
+  await bitacora.save();
+
   res.send(model);
 });
 
