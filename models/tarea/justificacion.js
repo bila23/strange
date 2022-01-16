cc0abd04b40bc1d84a2fe9f3dd23b759466f9054
Bila <wgbila@gmail.com>
2022-01-16 10:33:27 -0600

    se agrego atributo nueva fecha

--- models/tarea/justificacion.js
+++ models/tarea/justificacion.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const schema = new mongoose.Schema({
   tarea: [{ type: Schema.Types.ObjectId, ref: "tarea" }],
   fechaAnterior: Date,
-
+  nuevaFecha: Date,
   descripcion: String,
   usuario_crea: String,
   fecha_crea: {
