030f7e96492e5a7b444edf867b5845ccc341f173
Bila <wgbila@gmail.com>
2022-01-30 18:49:51 -0600

    ...

---
+++ services/NotasService.js
@@ -0,0 +1,39 @@
+const _ = require("lodash");
+const { Tarea } = require("../models/tarea/tarea");
+
+//recupero las tareas del dia
+//separo las tareas en los diferentes usuarios que hay
+//verificar que pasa si no hay usuarios
+//en el caso que si haya usuarios recuperar sus tareas
+//ver cuantas tareas han finalizado y cuantas tiene pendiente
+//calculo de nota
+
+async function separeteInUser() {
+  const tareas = await findTodayTareas();
+  if (tareas.length === 0) return null;
+  const result = _.uniq(tareas, function (model) {
+    return model.responsable;
+  });
+  console.log(result);
+}
+
+async function findTodayTareas() {
+  const now = new Date();
+
+  const result = await Tarea.aggregate()
+    .addFields({
+      year: { $year: "$fechaFin" },
+      month: { $month: "$fechaFin" },
+      day: { $dayOfMonth: "$fechaFin" },
+    })
+    .match({
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate(),
+    });
+
+  return result;
+}
+
+exports.findTodayTareas = findTodayTareas;
+exports.separeteInUser = separeteInUser;
---
+++ tests/NotasService.test.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const NotasService = require("../services/NotasService");
+const { MONGO_URL } = require("../start/db_url");
+
+describe("Funcionalidad relacionada a notas", () => {
+  beforeAll(async () => {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  it("Recupero las tareas de este dia", async () => {
+    const result = await NotasService.findTodayTareas();
+    expect(result.length).toBeGreaterThanOrEqual(0);
+  });
+
+  it("Separo los usuarios", async () => {
+    await NotasService.separeteInUser();
+    //expect(result.length).toBeGreaterThanOrEqual(0);
+  });
+});
