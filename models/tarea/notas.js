0d58b133acdeea7146604b456e0c389bbfa24a8f
Bila <wgbila@gmail.com>
2022-02-05 17:55:38 -0600

    se agregaron nuevos campos

--- models/tarea/notas.js
+++ models/tarea/notas.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 const notasSchema = new mongoose.Schema({
   usuario: { type: Schema.Types.ObjectId, ref: "usuario" },
   fecha: Date,
+  day: Number,
+  month: Number,
+  year: Number,
   resultado: Number,
   finalizadas: Number,
   pendientes: Number,
