74d3063a94d433454c61e5b935fe881a1f66e797
Bila <wgbila@gmail.com>
2022-02-05 18:09:52 -0600

    notas job

--- jobs/tareaCron.js
+++ jobs/tareaCron.js
@@ -1,5 +1,31 @@
 const cron = require("node-cron");
 const TareaService = require("../services/TareaService");
+const NotasService = require("../services/NotasService");
+
+/*
+ # ┌────────────── second (optional)
+ # │ ┌──────────── minute
+ # │ │ ┌────────── hour
+ # │ │ │ ┌──────── day of month
+ # │ │ │ │ ┌────── month
+ # │ │ │ │ │ ┌──── day of week
+ # │ │ │ │ │ │
+ # │ │ │ │ │ │
+ # * * * * * *
+field	        value
+second	      0-59
+minute	      0-59
+hour	        0-23
+day of month	1-31
+month	        1-12 (or names)
+day of week	  0-7 (or names, 0 or 7 are sunday)
+*/
+
+function notas() {
+  cron.schedule("* 0 10 * * *", async () => {
+    await NotasService.execute();
+  });
+}
 
 function tareas_proc_aprob_to_pend() {
   cron.schedule("0 2 * * *", async () => {
